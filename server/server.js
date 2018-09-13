64d3348491b1d699b89525b4e26eaf918a6a87be
Aaron Kim <aaron@Aarons-MacBook-Pro.local>
2018-09-13 15:47:44 -0600

    message

--- server/server.js
+++ server/server.js
@@ -14,10 +14,11 @@ const express = require('express'),
 
 const app = express()
 app.use(bodyParser.json())
-app.use(express.static(`${__dirname}/../build`))
-// app.get('*', (req,res)=>{
-//     res.sendFile(path.join(__dirname, '../build/index.html'))
-// })
+// app.use(express.static(`${__dirname}/../build`))
+
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'))
+})
 
 
 
